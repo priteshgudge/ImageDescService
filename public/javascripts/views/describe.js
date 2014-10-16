e91264511062c401315035b7f5cac6bc21b480a1
Meghan Larson <meghanlarson@benetech.org>
2014-10-16 16:03:44 -0400

    IMG-715 fix choose button

--- public/javascripts/views/describe.js
+++ public/javascripts/views/describe.js
@@ -9,22 +9,14 @@ define([
   var DescribeView = Backbone.View.extend({
     //div.
     tagName:  "div",
-
-    // The DOM events specific to an item.
-    events: {
-      "click #play_again_button" : "closeDecisionTree"
-    },
     
     // Render the recommendation.
     render: function() {
       var compiledTemplate = _.template( describeTemplate, {answer: this.model});
       this.$el.html(compiledTemplate);
       return this;
-    },
-
-    closeDecisionTree: function() {
-      $("#questionnaire").modal("hide");
     }
+    
   });
   return DescribeView;
 });
