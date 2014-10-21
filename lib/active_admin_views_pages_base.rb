708ca9f3c142e237dec46bd2e3a841d0ce7cc60b
Meghan Larson <meghanlarson@benetech.org>
2014-10-21 13:30:28 -0400

    IMG-678 use the same layout for regular and admin pages. Make header nav consistent

--- lib/active_admin_views_pages_base.rb
+++ lib/active_admin_views_pages_base.rb
@@ -4,12 +4,11 @@ class ActiveAdmin::Views::Pages::Base < Arbre::HTML::Document
 
   # Renders the content for the footer
   def build_footer
-    div :id => "footer" do
-      para "Legal Disclaimer: The Poet tool is intended to assist in the creation of accessible content. Use of the Poet tool is limited to activities for creating content that comply with applicable copyright law and contractual agreements."
-      para "Copyright &copy; #{Date.today.year.to_s} #{link_to('Benetech', 'http://www.benetech.org')}.".html_safe
-      
-    end
+    render "shared/footer"
   end
 
+  def build_header
+  	render "shared/header"
+  end
 end
 
