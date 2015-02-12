9fcd6491de4636b633ef56aa59eea097784a4af5
Meghan Larson <meghanlarson@benetech.org>
2015-02-12 12:15:43 -0500

    Fix reference

--- app/models/image_category.rb
+++ app/models/image_category.rb
@@ -2,7 +2,7 @@ class ImageCategory < ActiveRecord::Base
   def as_json(options = {})
     json = super(options)
     json['id'] = self.id
-    json['name'] = self.id
+    json['name'] = self.name
     json
   end
 end
