399a614fe9e03208a79036716a781e43d305f4b4
John Brugge <john.brugge@gmail.com>
2015-02-19 13:23:39 -0600

    IMG-824: Failing test.

---
+++ test/unit/helpers/daisy_book_helper_test.rb
@@ -0,0 +1,28 @@
+require "test/unit"
+
+class DaisyBookHelperTest < Test::Unit::TestCase
+  def setup
+    @helper = DaisyBookHelper::BatchHelper.new
+    @library = Library.find(1)
+  end
+  
+  def test_alt_insertion
+    # Set up the data
+    xml = File.read('features/fixtures/BookXMLWithImagesWithOurProdnotes.xml')
+    
+    new_alt = Alt.create(:alt => "this is new alt text", :dynamic_image_id => 3)
+    
+    # Run the test
+    doc_string = @helper.get_contents_with_updated_descriptions(xml, @library)
+    
+    # Check the result
+    doc = Nokogiri::XML doc_string
+    imgs = doc.xpath("//xmlns:img")
+    assert_equal 1, imgs.size, "Image count in test file"
+    
+    imgs.each do | img |
+      alt = img['alt']
+      assert_equal "this is new alt text", alt, "Updated alt text"
+    end
+  end
+end
