4563bb6892ae4ee3832ffed9934321df9797227d
John Brugge <john.brugge@gmail.com>
2015-02-19 16:42:53 -0600

    IMG-824: Add test for alt text that wasn't from a Poet user.

--- test/unit/helpers/daisy_book_helper_test.rb
+++ test/unit/helpers/daisy_book_helper_test.rb
@@ -4,18 +4,17 @@ class DaisyBookHelperTest < Test::Unit::TestCase
   def setup
     @helper = DaisyBookHelper::BatchHelper.new
     @library = Library.find(1)
+    @xml = File.read('features/fixtures/BookXMLWithImagesWithOurProdnotes.xml')
   end
   
   def test_alt_insertion
     # Set up the data
-    xml = File.read('features/fixtures/BookXMLWithImagesWithOurProdnotes.xml')
-    
     new_alt = Alt.create(:alt => "this is new alt text", 
             :dynamic_image_id => 3, 
                  :from_source => false)
     
     # Run the test
-    doc_string = @helper.get_contents_with_updated_descriptions(xml, @library)
+    doc_string = @helper.get_contents_with_updated_descriptions(@xml, @library)
     
     # Check the result
     doc = Nokogiri::XML doc_string
@@ -24,7 +23,29 @@ class DaisyBookHelperTest < Test::Unit::TestCase
     
     imgs.each do | img |
       alt = img['alt']
-      assert_equal "this is new alt text", alt, "Updated alt text"
+      assert_equal new_alt.alt, alt, "Updated alt text"
+    end
+  end
+  
+  def test_skip_alt
+    # Set up the data
+    # Say Alt is original soure, so shouldn't be text to update
+    Alt.update_all({ :from_source => true })
+    new_alt = Alt.create(:alt => "this is new alt text", 
+            :dynamic_image_id => 3, 
+                 :from_source => true)
+    
+    # Run the test
+    doc_string = @helper.get_contents_with_updated_descriptions(@xml, @library)
+    
+    # Check the result
+    doc = Nokogiri::XML doc_string
+    imgs = doc.xpath("//xmlns:img")
+    assert_equal 1, imgs.size, "Image count in test file"
+    
+    imgs.each do | img |
+      alt = img['alt']
+      assert_not_equal new_alt.alt, alt, "Updated alt text"
     end
   end
 end
