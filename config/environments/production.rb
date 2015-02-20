c40a5cecb38d2513589d2e068bd408c6cf0dd272
Meghan Larson <meghanlarson@benetech.org>
2015-02-20 15:45:10 -0500

    add to production for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ DiagramRailsApp::Application.configure do
                                :s3_credentials => {:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']},
                                :bucket => ENV['POET_ASSET_BUCKET']}
 
+  #MathML Cloud subscription key
+  MATHML_CLOUD_BASE_PATH = "api.staging.mathmlcloud.org"
+  MATHML_CLOUD_SUBSCRIPTION_KEY = "2dad839e04f2400eb3f96bbe3559044d"
 end
