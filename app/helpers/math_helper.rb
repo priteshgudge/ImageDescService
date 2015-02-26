4826fe9c8a29671822be1570c5e8a8d7314f621e
John Brugge <john.brugge@gmail.com>
2015-02-26 11:28:26 -0600

    Adding RDoc

--- app/helpers/math_helper.rb
+++ app/helpers/math_helper.rb
@@ -1,3 +1,5 @@
+# Common methods for manipulating a document to comply with
+# MathML requirements.
 module MathHelper
   MATHML_NS = 'http://www.w3.org/1998/Math/MathML'
   MATHML_EXTENSION = 'z39-86-extension-version'
@@ -24,7 +26,9 @@ module MathHelper
   MATHML_2_EXTERNAL_ID = '-//W3C//DTD MathML 2.0//EN'
   MATHML_ENTITY_DECL_TYPE = 4
   DTD_ENTITY_DECL_TYPE = 5
-    
+
+  # Adds meta tags required for DAISY to the given file 
+  # if MathML is detected in _contents_xml_.
   def self.get_opf_contents_for_math(filename, contents_xml)
     file = File.new(filename)
     doc = Nokogiri::XML file
@@ -54,19 +58,25 @@ module MathHelper
     return doc.to_xml
   end
 
+  # Returns a new Fragment from the contents of Equation#element.
   def self.create_math_element(equation)
     fragment = Nokogiri::XML.fragment equation.element
     return fragment.children.first
   end
   
+  # Replaces the given _image_element_ Node in the document
+  # with _math_element_, where _image_source_ is the path to
+  # actual image file in the book package.
   def self.replace_math_image(image_element, math_element, image_source)
     math_element.parent = image_element.parent
     image_element.unlink
     math_element['altimg'] = image_source
   end
   
+  # Add the set of required MathML DTD extensions to the given _doc_.
+    # For description of DTD extensions, see http://www.daisy.org/projects/mathml/mathml-in-daisy-spec.html#h_23  
   def self.attach_math_extensions(doc)
-    # For description of DTD extensions, see http://www.daisy.org/projects/mathml/mathml-in-daisy-spec.html#h_23
+    # Make sure we only try to add them once
     if (doc.internal_subset.children.none? { |elt| elt.name == "MATHML.prefixed" })
       Nokogiri::XML::EntityDecl.new("MATHML.prefixed", doc, MATHML_ENTITY_DECL_TYPE, nil, nil, "INCLUDE")
       Nokogiri::XML::EntityDecl.new("MATHML.prefix", doc, MATHML_ENTITY_DECL_TYPE, nil, nil, "m")
@@ -78,6 +88,7 @@ module MathHelper
     return doc
   end
   
+  # Returns true if there are any MathML elements in the XML string.
   def self.contains_math(content_string)
     doc = Nokogiri::XML content_string
     return doc.xpath('//m:math', 'm' => MATHML_NS).size > 0
