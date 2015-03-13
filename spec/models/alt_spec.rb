42016c5cdff470e2a06b068fa0e2c0662ca5ab38
John Brugge <john.brugge@gmail.com>
2015-03-13 10:08:35 -0500

    IMG-754: Work on first real model spec.

--- spec/models/alt_spec.rb
+++ spec/models/alt_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Alt do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    Factory.create(:alt).should be_valid
+  end
+  
+  it "can be blank text"
+  it "has a maximum length"
+  it "is invalid without a dynamic image id"
 end
