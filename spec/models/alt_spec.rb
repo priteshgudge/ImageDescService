c01bb6e851efd616dd8948a3c60cfed780aaf572
John Brugge <john.brugge@gmail.com>
2015-03-13 10:19:27 -0500

    IMG-754: Alt validation specs.

--- spec/models/alt_spec.rb
+++ spec/models/alt_spec.rb
@@ -5,7 +5,16 @@ describe Alt do
     Factory.create(:alt).should be_valid
   end
   
-  it "can be blank text"
-  it "has a maximum length"
-  it "is invalid without a dynamic image id"
+  it "can be blank text" do
+    Factory.create(:alt, alt: "").should be_valid
+  end
+  
+  it "has a maximum length" do
+    long_description = "Text that is longer than 255. Text that is longer than 255. Text that is longer than 255. Text that is longer than 255. Text that is longer than 255. Text that is longer than 255. Text that is longer than 255. Text that is longer than 255. Text that is longer than 255. "
+    Factory.build(:alt, alt: long_description).should_not be_valid
+  end
+  
+  it "is invalid without a dynamic image id" do
+    Factory.build(:alt, dynamic_image: nil).should_not be_valid
+  end
 end
